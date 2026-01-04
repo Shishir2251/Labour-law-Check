c6d7c2a8e589269edc69570db72ada6a4d45c6c6
Shishir2251 <emdadul2251@gmail.com>
2026-01-04 14:16:39 +0600

    initial commit

---
+++ app/utils/loaders.py
@@ -0,0 +1,11 @@
+from langchain_community.document_loaders import PyPDFLoader, TextLoader, Docx2txtLoader
+
+def load_document(path: str):
+    if path.endswith(".pdf"):
+        return PyPDFLoader(path).load()
+    elif path.endswith(".txt"):
+        return TextLoader(path).load()
+    elif path.endswith(".docx"):
+        return Docx2txtLoader(path).load()
+    else:
+        raise ValueError("Unsupported file type")
